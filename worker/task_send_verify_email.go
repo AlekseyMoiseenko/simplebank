55d0fb05d09a34a21ac4d3c18818bacde609162e
Aleksey <aleksey.moiseenko79@gmail.com>
2026-06-29 22:57:00 +0300

    create verify email and send it

--- worker/task_send_verify_email.go
+++ worker/task_send_verify_email.go
@@ -5,11 +5,19 @@ import (
 	"encoding/json"
 	"fmt"
 
+	db "github.com/AlekseyMoiseenko/simplebank/db/sqlc"
+	"github.com/AlekseyMoiseenko/simplebank/util"
 	"github.com/hibiken/asynq"
 	"github.com/rs/zerolog/log"
 )
 
-const TaskSendVerifyEmail = "task:send_verify_email"
+const (
+	TaskSendVerifyEmail = "task:send_verify_email"
+
+	templateName   = "send_verify_email_template"
+	secretCodeSize = 32
+	verifyUrl      = "http://localhost:8080/v1/verify_email"
+)
 
 type PayloadSendVerifyEmail struct {
 	Username string `json:"username"`
@@ -51,7 +59,28 @@ func (processor *RedisTaskProcessor) ProcessTaskSendVerifyEmail(ctx context.Cont
 		return fmt.Errorf("failed to get user: %w", err)
 	}
 
-	//TODO: send email to user
+	verifyEmail, err := processor.store.CreateVerifyEmail(ctx, db.CreateVerifyEmailParams{
+		Username:   user.Username,
+		Email:      user.Email,
+		SecretCode: util.RandomHash(secretCodeSize),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create verify email: %w", err)
+	}
+
+	subject := "Simple Bank"
+	verifyUrl := fmt.Sprintf("%s?email_id=%d&secret_code=%s", verifyUrl, verifyEmail.ID, verifyEmail.SecretCode)
+	content := fmt.Sprintf(`Hello %s,<br/>
+	Thank you for registering!<br/>
+	Please <a href="%s">click here</a> to verify your email address.<br/>
+	`, user.FullName, verifyUrl)
+	to := []string{user.Email}
+
+	err = processor.mailer.SendEmail(templateName, subject, content, to, nil)
+	if err != nil {
+		return fmt.Errorf("failed to send verify email: %w", err)
+	}
+
 	log.Info().Str("Type", task.Type()).Bytes("payload", task.Payload()).
 		Str("email", user.Email).Msg("processed task")
 
